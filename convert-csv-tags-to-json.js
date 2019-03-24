772d15e7d3405e9afd76ab7aca02f2147b758033
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-03-24 21:22:43 +0100

    Add last script

---
+++ convert-csv-tags-to-json.js
@@ -0,0 +1,34 @@
+const R = require('ramda');
+const {getFileContentAsString, writeJson} = require('./lib/file');
+
+const isNotEmpty = str => R.not(R.equals('')(str));
+
+const haveMoreThan3Sentences = obj => R.pipe(
+  R.prop('sentences'),
+  R.length,
+  R.gte(3),
+  R.not,
+)(obj);
+
+const convertSentencesToArray = obj => R.pipe(
+  R.prop('sentences'),
+  R.split(','),
+  R.filter(isNotEmpty),
+  R.assoc('sentences', R.__, obj)
+)(obj);
+
+const process = R.pipe(
+  getFileContentAsString,
+  R.split('\n'),
+  R.map(R.pipe(
+    R.replace(/,/, '<delim>'),
+    R.split('<delim>')
+  )),
+  R.map(R.zipObj(['tag', 'sentences'])),
+  R.map(convertSentencesToArray),
+  R.filter(haveMoreThan3Sentences),
+  R.tap(console.log),
+  writeJson('out/res-mot-sens-proche.json'),
+);
+
+process('input/res-mot-sens-proche.csv');
