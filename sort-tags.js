97378766a0f1f2d84b20f44ebc645d795e5161ba
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-03-24 22:07:05 +0100

    last correction

--- sort-tags.js
+++ sort-tags.js
@@ -2,9 +2,7 @@ const R = require('ramda');
 const fs = require('fs-extra');
 const Bromise = require('bluebird');
 const {getSentimentsArray} = require('./lib/sentiment');
-
-const input = fs.readJsonSync('out/data_non_sentiment.json');
-const writeJSON = R.curry((file, data) => fs.writeJsonSync(file, data));
+const {readJson, writeJsonToCsv, writeJson} = require('./lib/file');
 
 const getSentimentAverage = R.pipe(
 	R.prop('sentences'),
@@ -20,12 +18,14 @@ const addSentimentAverage = obj => R.pipe(
 const mapPromise = R.curry((f, x) => Bromise.map(x, f));
 
 const process = R.pipe(
+	readJson,
 	mapPromise(addSentimentAverage),
 	R.then(R.pipe(
 		R.sort(R.ascend(R.prop('sentiment'))),
 		R.tap(console.log),
-		writeJSON('out/blob.json')
+		R.tap(writeJsonToCsv('out/test-csv.csv')),
+		writeJson('out/res-with-sentences.json')
 	))
 );
 
-process(input);
+process('out/data_non_sentiment.json');
